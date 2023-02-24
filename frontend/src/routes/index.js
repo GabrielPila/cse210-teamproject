d492be6ed03daaafcbbe3e0e8ad6a6fe2103c846
lixuechun <318112194@qq.com>
2023-02-24 11:00:05 -0800

    remove jsx file

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,9 +3,7 @@ import ReviewPage from '../pages/ReviewPage.js';
 import SingleListingPage from '../pages/SingleListingPage.js';
 import Login from '../pages/Login.js';
 import SearchPage from '../pages/SearchPage.js';
-// import Signup from '../pages/Signup.js';
-
-import Signup from '../pages/signUp.jsx'; // jsx file for signUp
+import Signup from '../pages/Signup.js';
 
 
 // the path should be same with django urls so that they can be rendered 

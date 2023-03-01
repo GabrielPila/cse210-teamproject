9efad355ce550c774efc9c2e3917bc4f0296de8d
lixuechun <318112194@qq.com>
2023-02-28 17:55:34 -0800

    link to backend and post/get data

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,26 +1,60 @@
-import "../styles/Login.css";
+import "../styles/Signup.css";
 import LoginNavbar from "../components/LoginNavbar";
-import {useNavigate} from 'react-router-dom'
+import { Navigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+import {register} from '../actions/auth'
+import {connect} from 'react-redux'
 
-function Signup() {
-  const navigate = useNavigate();
 
-  const navigateToSearch = () => {
-    navigate('/search');
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [accountCreated, setAccountCreated] = useState(false);
+  const { username, password, email } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    register(username, password, email);
+    setAccountCreated(true);
   };
 
+  if (accountCreated) return <Navigate to="/login/" />;
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" onSubmit={(e) => onSubmit(e)}>
         <div className="Auth-form-content">
           <LoginNavbar />
-          <h3 className="Welcome-to-housing">Welcome to Housing Safari in San Diego</h3>
-          <div className="form-email">
+          <h3 className="Welcome-to-housing">
+            Welcome to Housing Safari in San Diego
+          </h3>
+
+          <div className="form-email ">
             <label>Email</label>
             <input
               type="email"
               className="placeholder"
+              placeholder="Enter your email..."
+              name="email"
+              onChange={(e) => onChange(e)}
+              value={email}
+              required
+            />
+          </div>
+          <div className="form-user">
+            <label>Username</label>
+            <input
+              type="text"
+              className="placeholder"
               placeholder="Enter your user name..."
+              name="username"
+              onChange={(e) => onChange(e)}
+              value={username}
+              required
             />
           </div>
           <div className="form-password">
@@ -28,16 +62,57 @@ function Signup() {
             <input
               type="password"
               className="placeholder"
-              placeholder="Enter password..."
+              placeholder="Enter your password..."
+              name="password"
+              onChange={(e) => onChange(e)}
+              value={password}
+              required
+              minLength="6"
             />
           </div>
-          <button type="Login" className="login-button" onClick={navigateToSearch}>
-            <a >Sign Up</a>
-          </button>
+          <button type="submit" className="login-button" >Register</button>
         </div>
       </form>
     </div>
-  );
-}
+  )
+};
 
-export default Signup;
+// function Signup() {
+//   const navigate = useNavigate();
+
+//   const navigateToSearch = () => {
+//     navigate('/search');
+//   };
+
+//   return (
+//     <div className="Auth-form-container">
+//       <form className="Auth-form">
+//         <div className="Auth-form-content">
+//           <LoginNavbar />
+//           <h3 className="Welcome-to-housing">Welcome to Housing Safari in San Diego</h3>
+//           <div className="form-email">
+//             <label>Email</label>
+//             <input
+//               type="email"
+//               className="placeholder"
+//               placeholder="Enter your user name..."
+//             />
+//           </div>
+//           <div className="form-password">
+//             <label>Password</label>
+//             <input
+//               type="password"
+//               className="placeholder"
+//               placeholder="Enter password..."
+//             />
+//           </div>
+//           <button type="Login" className="login-button" onClick={navigateToSearch}>
+//             <a >Sign Up</a>
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+export default Signup;// connect(null, {register})(Signup);
